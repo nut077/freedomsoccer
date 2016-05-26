7ea26ef47ea91e75a3d4c41c5072e02ca0bbbd4f
nutfreedom <nuttapon.tavee@gmail.com>
2016-05-26 08:30:28 +0700

    new

---
+++ src/main/java/com/freedomsoccer/FreedomsoccerApplication.java
@@ -0,0 +1,12 @@
+package com.freedomsoccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreedomsoccerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreedomsoccerApplication.class, args);
+	}
+}
