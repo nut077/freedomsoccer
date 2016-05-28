b05b55d618db8c7cc28ea13e254e2b95991f6445
nutfreedom <nuttapon.tavee@gmail.com>
2016-05-28 22:27:51 +0700

    user

--- src/main/java/com/freedomsoccer/controller/RegisterController.java
+++ src/main/java/com/freedomsoccer/controller/RegisterController.java
@@ -1,16 +1,18 @@
 package com.freedomsoccer.controller;
 
+import com.freedomsoccer.domain.Role;
 import com.freedomsoccer.domain.User;
 import com.freedomsoccer.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class RegisterController {
@@ -24,10 +26,15 @@ public class RegisterController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String save(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+    public String save(@Valid User user, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "auth/register";
         } else {
+            Role role = new Role();
+            role.setName("ADMIN");
+            role.setRole("ROLE_ADMIN");
+            List<Role> roleList = Arrays.asList(role);
+            user.setRoles(roleList);
             User user1 = userServiceImpl.save(user);
             return "auth/register";
         }
